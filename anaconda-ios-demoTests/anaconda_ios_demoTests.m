4ef8660b4a466567b3d4c33dc3b6dce307133c69
jeffmcfadden <jeff@forgeapps.com>
2014-05-22 13:51:53 -0700

    Initial Commit

---
+++ anaconda-ios-demoTests/anaconda_ios_demoTests.m
@@ -0,0 +1,34 @@
+//
+//  anaconda_ios_demoTests.m
+//  anaconda-ios-demoTests
+//
+//  Created by Jeff McFadden on 5/22/14.
+//  Copyright (c) 2014 ForgeApps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface anaconda_ios_demoTests : XCTestCase
+
+@end
+
+@implementation anaconda_ios_demoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
